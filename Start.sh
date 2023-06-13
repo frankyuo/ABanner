ca441d21ddf8ec58de08b086f50527f95b34babd
frankyuo <132846294+frankyuo@users.noreply.github.com>
2023-06-12 22:23:40 -0300

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,19 @@
+#¡ bin/bash/
+clear
+pkg install git -y
+pkg install ruby -y
+pkg install neofetch -y
+mv bash1.bashrc ~/../usr/etc
+cd 
+cd ..
+cd usr
+cd etc
+rm motd
+rm bash.bashrc
+mv bash1.bashrc bash.bashrc
+clear
+figlet -f big ABanner | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
