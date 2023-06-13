4cb1ecd0afed465807a229fd4d6d5ed94516d2e6
frankyuo <132846294+frankyuo@users.noreply.github.com>
2023-06-12 22:34:49 -0300

    Create Remove.sh

---
+++ Remove.sh
@@ -0,0 +1,15 @@
+#¡ bin/bash/
+clear
+mv bash.bashrc ~/../usr/etc
+cd 
+cd ..
+cd usr
+cd etc
+rm bash.bashrc
+clear
+figlet -f big ABanner Removed | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+exit
